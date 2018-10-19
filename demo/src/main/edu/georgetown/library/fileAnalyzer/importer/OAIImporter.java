e2ca742f237e3e75b8dc1a852e19961231ea7208
Terry Brady <terrywbrady@gmail.com>
2018-10-19 16:30:37 -0700

    enhance oai crawler

--- demo/src/main/edu/georgetown/library/fileAnalyzer/importer/OAIImporter.java
+++ demo/src/main/edu/georgetown/library/fileAnalyzer/importer/OAIImporter.java
@@ -18,10 +18,13 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -29,10 +32,13 @@ import org.xml.sax.SAXException;
 import edu.georgetown.library.fileAnalyzer.util.XMLUtil;
 
 public class OAIImporter extends DefaultImporter {
-    private enum Verbs {ListMetadataFormats,ListSets,ListRecords;}
+    private enum Verbs {ListMetadataFormats,ListSets,ListRecords,ListIdentifiers;}
     private static enum DemoStatsItems implements StatsItemEnum {
         Key(StatsItem.makeStringStatsItem("Key", 300)),
-        Value(StatsItem.makeStringStatsItem("Value", 400)),
+        Title(StatsItem.makeStringStatsItem("Title", 300)),
+        Identifier(StatsItem.makeStringStatsItem("Identifier")),
+        Repo(StatsItem.makeStringStatsItem("Repo").makeFilter(true)),
+        ObjType(StatsItem.makeStringStatsItem("ObjType").makeFilter(true)),
         ;
         
         StatsItem si;
@@ -42,7 +48,16 @@ public class OAIImporter extends DefaultImporter {
 	public static enum Generator implements StatsGenerator {
 		INSTANCE;
 
-		public Stats create(String key) {return new Stats(details, key);}
+		public Stats create(String key) {
+		    Stats s = new Stats(details, key);
+		    //This is currently hard-coded for the ArchivesSpace OAI Service
+	        Matcher m = Pattern.compile(".*/repositories/([^/]+)/([^/]+)/.*").matcher(key);
+	        if (m.matches()) {
+	            s.setVal(DemoStatsItems.Repo, m.group(1));
+                s.setVal(DemoStatsItems.ObjType, m.group(2));
+	        }
+		    return s;
+		}
 	}
 	
 	public static StatsItemConfig details = StatsItemConfig.create(DemoStatsItems.class);
@@ -73,62 +88,87 @@ public class OAIImporter extends DefaultImporter {
             for(String resumptionToken = ""; resumptionToken != null; resumptionToken = processRequest(types, url, resumptionToken));
         }
         
+        details.createFilters(types);
 		return new ActionResult(selectedFile, verb.toString(), this.toString(), details, types, true, timer.getDuration());
 	}
 	
+    public String getText(Node n) {
+        return getText(n, "");
+    }
+	public String getText(Node n, String def) {
+	    return n instanceof Element ? ((Element)n).getTextContent() : def;
+	}
+	
+    public String getText(NodeList nl, String def) {
+        return nl.getLength() > 0 ? getText(nl.item(0), def) : def;
+    }
+
+    public String getText(NodeList nl) {
+        return getText(nl, "");
+    }
+    
+    public Stats addStats(TreeMap<String,Stats> types, String val) {
+        Stats stat = Generator.INSTANCE.create(val);
+        types.put(val, stat);
+        return stat;
+    }
+    
+    public NodeList getNodes(Document d, Verbs verb) {
+        if (verb == Verbs.ListSets) {
+            return d.getElementsByTagName("setSpec");
+        } else if (verb == Verbs.ListMetadataFormats) {
+            return d.getElementsByTagName("metadataPrefix");
+        } else if (verb == Verbs.ListRecords) {
+            return d.getElementsByTagName("record");
+        } else if (verb == Verbs.ListIdentifiers) {
+            return d.getElementsByTagName("identifier");
+        } else {
+            return d.getElementsByTagName("xxxemptyxxx");            
+        }
+        
+    }
+
 	public String processRequest(TreeMap<String,Stats> types, String url, String resumptionToken) {
         Verbs verb = (Verbs)this.getProperty(VERB);
         String set = this.getProperty(SET).toString();
         String format = this.getProperty(FORMAT).toString();
+        String DC = "http://purl.org/dc/elements/1.1/";
 	    
         try {
             URIBuilder urib = new URIBuilder(url).addParameter("verb", verb.toString());
-            if (verb == Verbs.ListRecords) {
-                if (!resumptionToken.isEmpty()) {
-                    urib.addParameter("resumptionToken", resumptionToken);
-                } else {
-                    if (!set.isEmpty()) urib.addParameter("set", set);
-                    if (!format.isEmpty()) urib.addParameter("metadataPrefix", format);                    
-                }
+            if (!resumptionToken.isEmpty()) {
+                urib.addParameter("resumptionToken", resumptionToken);
+            } else {
+                if (!format.isEmpty()) urib.addParameter("metadataPrefix", format);
+                if (!set.isEmpty()) urib.addParameter("set", set);
             }
             URI uri = urib.build();
             System.out.println(uri);
             HttpURLConnection con= (HttpURLConnection) uri.toURL().openConnection();
             
             Document d = XMLUtil.db_ns.parse(con.getInputStream());
-            if (verb == Verbs.ListSets) {
-                NodeList nl = d.getElementsByTagName("setSpec");
-                for(int i=0; i< nl.getLength(); i++) {
-                    String val = ((Element)nl.item(i)).getTextContent();
-                    types.put(val, Generator.INSTANCE.create(val));
-                }
-            } else if (verb == Verbs.ListMetadataFormats) {
-                NodeList nl = d.getElementsByTagName("metadataPrefix");
-                for(int i=0; i< nl.getLength(); i++) {
-                    String val = ((Element)nl.item(i)).getTextContent();
-                    types.put(val, Generator.INSTANCE.create(val));
-                }
-                
-            } else {
-                NodeList nl = d.getElementsByTagName("record");
+            NodeList nl = getNodes(d, verb);
+            
+            if (verb == Verbs.ListRecords) {
                 for(int i=0; i< nl.getLength(); i++) {
                     Element rec = (Element)nl.item(i);
-                    NodeList nt = rec.getElementsByTagNameNS("http://purl.org/dc/elements/1.1/", "title");
-                    NodeList nid = rec.getElementsByTagName("identifier");
-                    String t = nt.getLength() > 0 ? ((Element)nt.item(0)).getTextContent() : "";
-                    String id = nid.getLength() > 0 ? ((Element)nid.item(0)).getTextContent() : "";
-                    Stats s = Generator.INSTANCE.create(id);
-                    s.setVal(DemoStatsItems.Value, t);
-                    types.put(id, s);
+                    String t = getText(rec.getElementsByTagNameNS(DC, "title"));
+                    String id = getText(rec.getElementsByTagName("identifier"));
+                    String dcid = getText(rec.getElementsByTagNameNS(DC, "identifier"));
+                    Stats s = this.addStats(types, id);
+                    s.setVal(DemoStatsItems.Title, t);
+                    s.setVal(DemoStatsItems.Identifier, dcid);
                     System.out.println(types.size() + "\t"+id+"\t"+t);
-                }
-                nl = d.getElementsByTagName("resumptionToken");
-                if (nl.getLength() > 0) {
-                    String rt = ((Element)nl.item(0)).getTextContent();
-                    System.out.println(rt);
-                    return rt.isEmpty() ? null : rt;
-                }
+                }                
+            } else {
+                for(int i=0; i< nl.getLength(); i++) {
+                    Stats s = addStats(types, getText(nl.item(i)));
+                    if (verb == Verbs.ListIdentifiers) {
+                    }
+                }                
             }
+            String rt = getText(d.getElementsByTagName("resumptionToken"));
+            return rt.isEmpty() || rt.endsWith("==") ? null : rt;
         } catch (URISyntaxException|IOException | SAXException e) {
             e.printStackTrace();
         }
